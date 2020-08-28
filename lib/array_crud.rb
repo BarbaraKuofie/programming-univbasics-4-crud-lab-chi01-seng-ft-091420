59bc9f48fbd6c985b5e1a3e9ee245881ded06c52
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-28 02:18:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  aarray.push ("element")
+  array.push ("beer")
 end
 
 def add_element_to_start_of_array(array, element)
